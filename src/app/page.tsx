e4704905ce6b8b09454ee62ac7cd78038b3ec55e
Mustafa Genc <eposta@mustafagenc.info>
2025-08-20 17:04:46 +0300

    Enhances layout and text alignment
    Refines the homepage layout for better visual appeal.
    
    It introduces a margin below the image and centers the main title.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="font-sans flex items-center flex-col justify-center min-h-screen p-8 pb-20 gap-3">
-      <div>
+      <div className="mb-5">
         <Image src="/logo.png" alt="alt" width={300} height={300} />
       </div>
-      <div className="text-7xl font-extrabold bg-gradient-to-tr from-amber-200 to-amber-400 text-transparent bg-clip-text">
+      <div className="text-7xl font-extrabold bg-gradient-to-tr from-amber-200 to-amber-400 text-transparent bg-clip-text text-center">
         Süreyya Alper
       </div>
     </div>
