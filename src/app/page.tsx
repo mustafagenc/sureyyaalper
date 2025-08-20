a3cc6a5b79f3663a8b308ac7e1d3a3f0aac5cb6e
Mustafa Genc <eposta@mustafagenc.info>
2025-08-20 16:09:39 +0300

    Adds a landing page
    Creates a basic landing page with a logo and welcome text.
    
    The page is designed to be visually appealing and welcoming.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      Süreyya Alper
+    <div className="font-sans flex items-center flex-col justify-center min-h-screen p-8 pb-20 gap-3">
+      <div>
+        <Image src="/logo.png" alt="alt" width={300} height={300} />
+      </div>
+      <div className="text-5xl text-shadow-2xs text-amber-200">Süreyya Alper</div>
     </div>
   );
 }
